95d7c6a43ddc822e7f287e50f6d41ec3259ae100
dackey-wav <danilkrasotov65@gmail.com>
2025-05-07 00:34:08 +0200

    add Element structure

--- PQHeap.h
+++ PQHeap.h
@@ -2,11 +2,30 @@
 #define PQHeap_H
 #include <utility>
 
+struct Element {
+    int e; //value
+    int p; //priority
+
+    Element(int value = 0, int priority = 0) : e(value), p(priority) {}
+    
+    bool operator>(const Element &other) const {
+        return p > other.p;
+    }
+
+    bool operator<(const Element &other) const {
+        return p < other.p;
+    }
+
+    bool operator==(const Element &other) const {
+        return p == other.p;
+    }
+};
+
 class PriorityQueue {
 public:
     virtual void insert(int e, int p) = 0;
-    virtual std::pair<int, int> extractMax() = 0;
-    virtual std::pair<int, int> peek() const = 0;
+    virtual Element extractMax() = 0;
+    virtual Element peek() const = 0;
     virtual void modifyKey(int e, int p) const = 0;
     virtual int returnSize() const = 0;
     virtual void clear() = 0;
@@ -14,7 +33,7 @@ public:
 };
 
 class HeapPriorityQueue : public PriorityQueue {
-    
+
 };
 
 #endif
